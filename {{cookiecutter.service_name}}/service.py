328470ad02e401cb080a56927f7d1fb2db2ce799
Scaletech.io <28793728+scaletechmaster@users.noreply.github.com>
2025-03-28 19:52:59 +0100

    Update service.py fix dictionary key for pycalrissian

--- {{cookiecutter.service_name}}/service.py
+++ {{cookiecutter.service_name}}/service.py
@@ -272,7 +272,7 @@ class SimpleExecutionHandler(ExecutionHandler):
     def get_secrets(self):
         logger.info("get_secrets")
         secrets={
-            "imagePullSecrets": self.local_get_file("/assets/pod_imagePullSecrets.yaml"),
+            "auths": self.local_get_file("/assets/pod_imagePullSecrets.yaml"),
             "additionalImagePullSecrets": self.local_get_file("/assets/pod_additionalImagePullSecrets.yaml")
         }
         return secrets
