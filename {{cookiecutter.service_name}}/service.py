cbd83d893ab866de4b13cff769567f3345d53149
Scaletech.io <28793728+scaletechmaster@users.noreply.github.com>
2025-05-16 23:33:30 +0200

    Update service.py

--- {{cookiecutter.service_name}}/service.py
+++ {{cookiecutter.service_name}}/service.py
@@ -211,8 +211,12 @@ class SimpleExecutionHandler(ExecutionHandler):
         logger.info("get_additional_parameters")
         additional_parameters: Dict[str, str] = {}
         additional_parameters = self.conf.get("additional_parameters", {})
-        
+        # params override
+        additional_parameters["aws_secret_access_key"] = os.getenv('MINIO_ACCESS_KEY')
+        additional_parameters["aws_access_key_id"] = os.getenv('MINIO_SECRET_KEY')
+        additional_parameters["endpoint_url"] = f"https://{os.getenv('MINIO_HOST')}"
         additional_parameters["s3_bucket"] = self.conf["lenv"]["Identifier"]
+
         additional_parameters["sub_path"] = self.conf["lenv"]["usid"]
 
         logger.info(f"additional_parameters: {additional_parameters.keys()}")
